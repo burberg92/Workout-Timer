16be01adc09e060625a6f3a7629cfb3567992337
your nameRodrigo <burberg92@gmail.com>
2023-09-03 16:55:02 -0500

    memoized

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import { useEffect, useState } from "react";
 import Calculator from "./Calculator";
 import ToggleSounds from "./ToggleSounds";
+import { useMemo } from "react";
+
+// Function to format a date object into a specific string format
+
+function formatTime(date) {
+  return new Intl.DateTimeFormat("en", {
+    month: "short",
+    year: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  }).format(date);
+}
 
 function App() {
   const [allowSound, setAllowSound] = useState(true);
   const [time, setTime] = useState(formatTime(new Date()));
 
-  // Will be be AM or PM
+  // Dervies whether it's AM or PM from the time 
   const partOfDay = time.slice(-2);
 
-  const workouts = [
+    // A list of workouts with a conditional for the number of exercises based on partOfDay (AM/PM)
+
+  const workouts = useMemo(()=> {
+    return [
     {
       name: "Full-body workout",
       numExercises: partOfDay === "AM" ? 9 : 8,
@@ -30,26 +46,25 @@ function App() {
       name: "Core only",
       numExercises: partOfDay === "AM" ? 5 : 4,
     },
-  ];
+    ];
+  }, [partOfDay])
 
-  function formatTime(date) {
-    return new Intl.DateTimeFormat("en", {
-      month: "short",
-      year: "2-digit",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    }).format(date);
-  }
+    // Function to format a date object into a specific string format
+
+
+
+  // useEffect hook to update the time state every second
 
   useEffect(function () {
-    const id = setInterval(function () {
-      setTime(formatTime(new Date()));
+    const id = setInterval(function () {  // setInterval runs the provided function every 1000ms (1 second)
+      setTime(formatTime(new Date()));      // Updates the time state every second with the new time
     }, 1000);
 
-    return () => clearInterval(id);
-  }, []);
+    return () => clearInterval(id);  // Cleanup function to clear the interval when the component is unmounted
+  }, []); // Empty dependency array means the effect runs once when the component mounts
 
+
+  // The rendered component
   return (
     <main>
       <h1>Workout timer</h1>
@@ -59,5 +74,5 @@ function App() {
     </main>
   );
 }
-
+// Exports the App component to be used in other parts of the application
 export default App;
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import clickSound from './ClickSound.m4a';
+import { memo } from 'react';
 
 function Calculator({ workouts, allowSound }) {
   const [number, setNumber] = useState(workouts.at(0).numExercises);
@@ -7,15 +8,35 @@ function Calculator({ workouts, allowSound }) {
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
 
-  const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
-  const mins = Math.floor(duration);
-  const seconds = (duration - mins) * 60;
+  const [duration, setDuration] = useState(0);
 
-  const playSound = function () {
+  const playSound = useCallback(function () {
     if (!allowSound) return;
     const sound = new Audio(clickSound);
     sound.play();
-  };
+  }, [allowSound])
+
+  useEffect(
+    function(){
+      setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak)
+    }, [number, sets,speed, durationBreak , playSound]
+  )
+
+  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  const mins = Math.floor(duration);
+  const seconds = (duration - mins) * 60;
+
+
+
+  function handleInc(){
+    setDuration((duration) => Math.floor(duration) + 1)
+    playSound();
+  }
+
+  function handleDec() {
+    setDuration((duration) => Math.ceil(duration) - 1)
+    playSound()
+  }
 
   return (
     <>
@@ -66,16 +87,16 @@ function Calculator({ workouts, allowSound }) {
         </div>
       </form>
       <section>
-        <button onClick={() => {}}>–</button>
+        <button onClick={handleDec}>–</button>
         <p>
           {mins < 10 && '0'}
           {mins}:{seconds < 10 && '0'}
           {seconds}
         </p>
-        <button onClick={() => {}}>+</button>
+        <button onClick={handleInc}>+</button>
       </section>
     </>
   );
 }
 
-export default Calculator;
+export default memo(Calculator);
